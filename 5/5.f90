80e53b8af282e6c73ec3d7e1fce5f16b8494c77d
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-09 16:46:14 -0600

    Added a fortran version of problem 5; running time is 1.918 s

---
+++ 5/5.f90
@@ -0,0 +1,26 @@
+program problem5
+implicit none
+
+integer (kind=8) :: x
+logical :: check
+x = 20*19
+do while (check(x).eqv..false.)
+    x=x+20
+enddo
+print*,"Final answer is",x
+
+end program
+
+logical function check (n)
+  implicit none
+  integer (kind=8) :: n,d
+  do d=1,20,1
+      if (mod(n,d).ne.0) then
+          check = .false.
+          return
+      endif
+  enddo
+  check = .true.
+  return
+end function check
+
