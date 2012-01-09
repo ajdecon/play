3cea1f0d988aa8af4e6871423d46813c85426a90
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-09 15:18:35 -0600

    Finished problem 4 successfully; biggest palindromic product of two 3-digit integers

--- 4/4.py
+++ 4/4.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 def is_palindrome(number):
     string = str(number)
     for i in range(len(string)/2):
@@ -8,7 +10,20 @@ def is_palindrome(number):
     return True
 
 # Test palindromes
-print is_palindrome(303)
-print is_palindrome(21)
-print is_palindrome(3467)
-print is_palindrome(9009)
+#print is_palindrome(303)
+#print is_palindrome(21)
+#print is_palindrome(3467)
+#print is_palindrome(9009)
+
+# Find largest palindrome from product of two three-digit numbers
+rmax = 999
+rmin = 100
+step = -1
+
+max = 0
+for x in range(rmax,rmin,step):
+    for y in range(rmax,rmin,step):
+        product = x*y
+        if is_palindrome(product) and product>max:
+            max = product
+print "Biggest palindromic product is %d\n" % (max)
