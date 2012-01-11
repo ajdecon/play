b499f86007bf3a7bc153378d863717b895235165
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-10 22:15:09 -0600

    Fixed problem 9 to check for the correct conditions, confirmed the answer. Runtime in Python is 0.374 s

--- 9/9.py
+++ 9/9.py
@@ -1,8 +1,10 @@
 import sys
-for a in range(1,32):
-    for b in range(1,32):
-        for c in range(1,32):
-            if ((a*a + b*b + c*c) == 1000) and (a<b<c):
-                print "a=%d, b=%d, c=%d" % (a,b,c)
-                print "abc = %d" % (a*b*c)
-                sys.exit()
+import math
+
+for a in range(1,1000):
+    for b in range(1,1000):
+        c = math.sqrt(a*a+b*b)
+        if (a+b+c==1000):
+            print "a=%d, b=%d, c=%d" % (a,b,c)
+            print "abc = %d" % (a*b*c)
+            sys.exit()
