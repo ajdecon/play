5cf7accc79667e699a7afcf04adb37d140e80a03
Adam DeConinck <ajdecon@ajdecon.org>
2012-01-20 19:04:01 -0600

    Wrote initial version of sieve algorithm; didn't work :(

--- 21/21.f90
+++ 21/21.f90
@@ -4,12 +4,44 @@
 
 program problem21
     implicit none
-    integer :: d 
-    if (d(220).eq.284) then
-        print*,"d(x) seems to work!"
-    else
-        print*,"back to drawing board"
-    endif
+    integer :: d, maxnum, a, b, total
+    parameter(maxnum = 9999)
+    logical, dimension(maxnum) :: sieve
+    
+    ! Initialize sieve to true
+    do a=1,maxnum,1
+        sieve(a)=.true.
+    enddo
+
+    ! Loop through all numbers up to maxnum.
+    ! For each number...
+    !  1) Calculate a=d(i). p
+    !  2) Calculate b=d(a)
+    !  3a) 
+    !  3b) else, both numbers are false.
+    do a=1,maxnum,1
+        ! Skip numbers we've already marked
+        if (sieve(a).eqv..false.) then
+            cycle
+        endif
+        ! Mark sieve as false if the numbers aren't amicable.
+        b = d(a)
+        if (d(b)/=a) then
+            sieve(a)=.false.
+            if (b<=maxnum) then
+                sieve(b)=.false.
+            endif
+        endif
+    enddo
+
+    total = 0
+    ! Sum all amicable numbers in the sieve
+    do a=1,maxnum,1
+        if (sieve(a).eqv..true.) then
+            total = total + a
+        endif
+    enddo
+    print*, "Total is",total
 end program
 
 ! Find the number of proper divisors of x
