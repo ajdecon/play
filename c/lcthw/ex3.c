993311c6d2844bf577c8e3cab6d577e62b2f847a
Adam DeConinck <ajdecon@ajdecon.org>
2012-12-30 12:14:16 -0800

    LCTHW exercise 3

---
+++ c/lcthw/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    int age = 28;
+    int height = 83;
+
+    printf("I am %d years old\n",age);
+    printf("I am %d inches tall\n",height);
+
+    return 0;
+}
