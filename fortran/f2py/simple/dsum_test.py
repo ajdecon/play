85010c48577602728babf536ea421450a156d3c3
Adam DeConinck <ajdecon@ajdecon.org>
2013-01-10 16:41:37 -0700

    Added tests for using an f2py module with Numpy arrays

--- fortran/f2py/simple/dsum_test.py
+++ fortran/f2py/simple/dsum_test.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy
 
 # Fortran module dsum.so, compiled with f2py -c dsum.pyf dsum.f
 from dsum import dsum
@@ -20,6 +21,14 @@ class TestDsum(unittest.TestCase):
     def testAddThreeFloats(self):
         in_tuple = (1.5,2.5,3.5,4.5,5.5)
         self.failUnless(dsum(in_tuple,3)==7.5)
+
+    def testAddNumpyArray(self):
+        in_array = numpy.array([1,2,3,4,5])
+        self.failUnless(dsum(in_array)==15.0)
+
+    def testAddPartialNumpyArray(self):
+        in_array = numpy.array([1,2,3,4,5])
+        self.failUnless(dsum(in_array,2)==3.0)
 
 def main():
     unittest.main()
